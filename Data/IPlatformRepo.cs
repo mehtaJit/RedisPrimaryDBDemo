afbe941d60b322dca99f61ab687d091e0254a130
Jitesh <Jitesh@LAPTOP-84I2SA1K>
2022-03-09 11:18:18 +0000

    Add project files.

---
+++ Data/IPlatformRepo.cs
@@ -0,0 +1,11 @@
+﻿using RedisPrimaryDBDemo.Models;
+
+namespace RedisPrimaryDBDemo.Data
+{
+    public interface IPlatformRepo
+    {
+        void CreatePlatform(Platform platform);
+        Platform GetPlatform(string id);
+        IEnumerable<Platform> GetAllPlatform();
+    }
+}
---
+++ Data/RedisPlatformRepo.cs
@@ -0,0 +1,23 @@
+﻿using RedisPrimaryDBDemo.Data;
+using RedisPrimaryDBDemo.Models;
+
+namespace RedisPrimaryDBDemo.Data
+{
+    public class RedisPlatformRepo : IPlatformRepo
+    {
+        public void CreatePlatform(Platform platform)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Platform> GetAllPlatform()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Platform GetPlatform(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
